04a7a044f7a9f1ff148e3552c401d70004d5fc48
sara kaplan <kap.sara@gmail.com>
2021-04-10 18:45:28 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderComponent from './components/headerComponent';
+import FooterComponent from './components/footerComponent';
+import ExchangeComponent from './components/exchangeComponent';
 
 function App() {
   return (
     <div className="App">
-  
+      <HeaderComponent/>
+      <ExchangeComponent/>
+      <FooterComponent/>
     </div>
   );
 }
