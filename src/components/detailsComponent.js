7ce6516e40930522c45283da3bef8d22f43a74dd
sara kaplan <kap.sara@gmail.com>
2021-04-12 17:23:51 +0300

    aaa

--- src/components/detailsComponent.js
+++ src/components/detailsComponent.js
@@ -14,7 +14,7 @@ export default function DetailsComponent(props) {
         <div className="mt-5">
             <h4 className="d-flex md-h4 justify-content-center text-center pt-5 text-muted" style={{fontSize: "3em"}}>
                 {parseFloat(fromFieldAmount).toFixed(2)} {selectedFromCurrency.substring('USD'.length)}{'\u00A0'}{'\u00A0'}
-                <img src="arrow-32.png" alt="arrows"/>{'\u00A0'}{'\u00A0'}
+                <img src="arrow-64.png" alt="arrows"/>{'\u00A0'}{'\u00A0'}
                 {parseFloat(toFieldAmount).toFixed(2)} {selectedToCurrency.substring('USD'.length)}
             </h4>
             <div className="d-flex justify-content-center pt-5 text-muted">
--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function FooterComponent() {
     return (
-        <footer>
-    
+        <footer className="px-5">
+        {'\u00a9'}Sarah Kaplan Shelest
         </footer>
     )
 }
